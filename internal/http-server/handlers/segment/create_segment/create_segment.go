52428bb8b238e08d87fc74e8b277342d5d2a4135
ghostik-gh <feodor200@mail.ru>
2023-08-26 03:15:16 +0300

    Create segments with percentages of people

--- internal/http-server/handlers/segment/create_segment/create_segment.go
+++ internal/http-server/handlers/segment/create_segment/create_segment.go
@@ -3,8 +3,12 @@ package create_segment
 import (
 	"avito_back_intership/internal/lib/logger/sl"
 	"avito_back_intership/internal/storage"
+	"database/sql"
 	"errors"
+	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"log/slog"
 
@@ -24,7 +28,18 @@ type Response struct {
 	resp.Response
 }
 
+// @Success			200		{object}	userModel.TokenAccessModel	"data"
+// @Failure			400,404	{object}	httpModel.ResponseMessage
+// @Failure			500		{object}	httpModel.ResponseMessage
+// @Failure			default	{object}	httpModel.ResponseMessage
+//
 //go:generate mockery --name=URLSaver
+type SegmentCreator interface {
+	CreateSegment(name string, amount string) error
+	UserList() (*sql.Rows, error)
+	CreateLog(user_id int, seg_name, opertaion string) error
+	CreateUserSegment(user_id int, segment string) error
+}
 
 // @Summary			Создание сегмента
 // @Tags			Segment
@@ -32,16 +47,8 @@ type Response struct {
 // @ID				segment-creation
 // @Accept			json
 // @Produce			json
-// @Param			input	body		Segment						true	"segment name"
-// @Success			200		{object}	userModel.TokenAccessModel	"data"
-// @Failure			400,404	{object}	httpModel.ResponseMessage
-// @Failure			500		{object}	httpModel.ResponseMessage
-// @Failure			default	{object}	httpModel.ResponseMessage
+// @Param			input	body		Request						true	"segment name"
 // @Router			/segment [post]
-type SegmentCreator interface {
-	CreateSegment(name string, amount string) error
-}
-
 func New(log *slog.Logger, segmentCreator SegmentCreator) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handlers.segment.create_segment.New"
@@ -63,29 +70,88 @@ func New(log *slog.Logger, segmentCreator SegmentCreator) http.HandlerFunc {
 
 		if err := validator.New().Struct(req); err != nil {
 			validateErr := err.(validator.ValidationErrors)
-
 			log.Error("invalid request", sl.Err(err))
-
 			render.JSON(w, r, resp.ValidationError(validateErr))
-
 			return
 		}
 
-		if err := segmentCreator.CreateSegment(req.Segment, "0"); err != nil {
+		amount, err := strconv.ParseFloat(req.Amount, 64)
+		if err != nil {
+			log.Info("amount not number ", sl.Err(err))
+		}
+		fmt.Printf("amount: %v\n", amount)
+		if amount <= 0 || req.Amount == "" || amount > 100 {
+			if err := segmentCreator.CreateSegment(req.Segment, "0"); err != nil {
+				if errors.Is(err, storage.ErrSegmentExists) {
+					log.Error(storage.ErrSegmentExists.Error(), slog.String("segment", req.Segment))
+					render.JSON(w, r, resp.Error(storage.ErrSegmentExists.Error()))
+					return
+				}
+				log.Error("failed to create segment", slog.String("segment", req.Segment))
+				render.JSON(w, r, resp.Error("failed to create segment"))
+				return
+			}
+		} else {
+			err := segmentCreator.CreateSegment(req.Segment, req.Amount)
 			if errors.Is(err, storage.ErrSegmentExists) {
 				log.Error(storage.ErrSegmentExists.Error(), slog.String("segment", req.Segment))
 				render.JSON(w, r, resp.Error(storage.ErrSegmentExists.Error()))
 				return
 			}
-			log.Error("failed to create segment", slog.String("segment", req.Segment))
-			render.JSON(w, r, resp.Error("failed to create segment"))
-			return
+			rows, err := segmentCreator.UserList()
+			if err != nil {
+				log.Error(err.Error())
+			}
+			var users []string
+			for rows.Next() {
+				var row string
+				rows.Scan(&row)
+				if err != nil {
+					log.Error(err.Error())
+				}
+				users = append(users, row)
+			}
+			var count int = int(float64(len(users)) * (amount / 100))
+			rand_users := chooseRandomUsers(users, count)
+			fmt.Printf("rand_users: %v\n", rand_users)
+			for _, v := range rand_users {
+				id, err := strconv.Atoi(v)
+				if err != nil {
+					log.Error(err.Error())
+				}
+				err = segmentCreator.CreateUserSegment(id, req.Segment)
+				if err != nil {
+					log.Error(err.Error())
+				}
+				err = segmentCreator.CreateLog(id, req.Segment, "add")
+				if err != nil {
+					log.Error(err.Error())
+				}
+			}
 		}
-		log.Info("segment created")
 
+		log.Info("segment created")
 		render.JSON(w, r, Response{
 			Response: resp.OK(),
 		})
 	}
 
+}
+
+func chooseRandomUsers(userIDs []string, count int) []string {
+	if count > len(userIDs) {
+		count = len(userIDs)
+	}
+	chosenUsers := make(map[string]bool)
+
+	for len(chosenUsers) < count {
+		randomIndex := rand.Intn(len(userIDs))
+		chosenUsers[userIDs[randomIndex]] = true
+	}
+	result := make([]string, 0, count)
+	for user := range chosenUsers {
+		result = append(result, user)
+	}
+
+	return result
 }
